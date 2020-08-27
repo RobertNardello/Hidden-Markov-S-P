7fa4e6d468da63af8857a6c3c375cff71291e932
RobertNardello <61129816+RobertNardello@users.noreply.github.com>
2020-08-26 20:58:00 -0400

    Add files via upload
    Hidden Markov model created in R & Python for the purpose of regime detection of S&P500 asset prices.

---
+++ hmm regime detection.R
@@ -0,0 +1,25 @@
+library('depmixS4')
+library('quantmod')
+
+rm(list=ls())
+
+#Obtain S&P500 data from 2010 onwards and
+# create the returns stream from this
+
+getSymbols( "^GSPC", from="2010-01-01" )
+#gspcRets = diff( log( Cl( GSPC ) ) )
+returns = GSPC$GSPC.Close
+plot(returns)
+
+# Fit a Hidden Markov Model with two states 
+# to the S&P500 returns stream
+hmm <- depmix(returns ~ 1, family = gaussian(), nstates = 2, data=data.frame(returns=returns))
+hmmfit <- fit(hmm, verbose = FALSE)
+post_probs <- posterior(hmmfit)
+
+# Plot the returns stream and the posterior
+# probabilities of the separate regimes
+layout(1:2)
+plot(returns, type='l', main='Regime Detection', xlab='', ylab='Returns')
+matplot(post_probs[,-1], type='l', main='Regime Posterior Probabilities', ylab='Probability')
+legend(x='bottomleft', c('Regime #1','Regime #2'), fill=1:2, bty='n')
